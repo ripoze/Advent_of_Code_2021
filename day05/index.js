7a92ff96d1a87c0e9fb91dc4ec9596fdd39baafb
juha <juha.ayvari@gmail.com>
2021-12-05 11:46:43 +0200

    day 5

--- day05/index.js
+++ day05/index.js
@@ -2,7 +2,7 @@ var data = require('fs').readFileSync('day05/input.txt', 'utf8')
 data = data.trim().split('\n')
 
 let lines = data.map(e => e.match(/\d+/g).map(Number))
-var arr = Array(1000).fill().map(() => Array(1000).fill(0))
+var grid = Array(1000).fill().map(() => Array(1000).fill(0))
 
 
 //Part 1
@@ -11,12 +11,12 @@ lines.map(line => {
     if (line[0] == line[2] || line[1] == line[3]) {
         for (let x = Math.min(line[0], line[2]); x <= Math.max(line[0], line[2]); x++) {
             for (let y = Math.min(line[1], line[3]); y <= Math.max(line[1], line[3]); y++) {
-                arr[y][x]++
+                grid[y][x]++
             }
         }
     }
 })
-console.log(`Part 1: ${arr.flat().filter(e => e > 1).length}`) //5294
+console.log(`Part 1: ${grid.flat().filter(e => e > 1).length}`) //5294
 
 
 //Part 2
@@ -25,12 +25,12 @@ lines.map(line => {
     if (line[0] != line[2] && line[1] != line[3]) {
         let x = line[0]
         let y = line[1]
-        arr[y][x]++
+        grid[y][x]++
             while (x != line[2]) {
                 x += line[0] > line[2] ? -1 : 1
                 y += line[1] > line[3] ? -1 : 1
-                arr[y][x]++
+                grid[y][x]++
             }
     }
 })
-console.log(`Part 2: ${arr.flat().filter(e => e > 1).length}`) //21698
+console.log(`Part 2: ${grid.flat().filter(e => e > 1).length}`) //21698
